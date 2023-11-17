ebfe5362559d7556a66fde8e47cebdc84661cd19
heipartho05 <heipartho72@gmail.com>
2023-11-17 18:18:02 +0600

    c++ skeleton added

---
+++ Basic/skeleton.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h> // all library are include
+// #include <iostream>
+using namespace std;
+
+int main()
+{
+    int x;
+    cout << "Enter any number: ";
+    cin >> x;
+    cout << "You enter " << endl << x;
+    return 0;
+}
