2b0a30f58b48f30cf143b08a462ac27ba0f7e4df
heipartho05 <heipartho72@gmail.com>
2023-11-18 15:59:35 +0600

    q7 modified

--- Pattern/Solution/q7.cpp
+++ Pattern/Solution/q7.cpp
@@ -15,9 +15,9 @@ int main()
         }
 
         // star
-        for (int j = 1; j <= i; j++)
+        for (int j = 1; j <= (2 * i) - 1; j++)
         {
-            cout << "* ";
+            cout << "*";
         }
         cout << endl;
     }
