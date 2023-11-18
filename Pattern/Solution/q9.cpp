7b5f5b883d44e8487c59f327dc1726d4f13128d7
heipartho05 <heipartho72@gmail.com>
2023-11-18 16:00:02 +0600

    q9 added

---
+++ Pattern/Solution/q9.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    for (int i = 1; i <= n; i++)
+    {
+        // space
+        for (int j = 1; j <= n - i; j++)
+        {
+            cout << " ";
+        }
+
+        // star
+        for (int j = 1; j <= (2 * i) - 1; j++)
+        {
+            cout << "*";
+        }
+        cout << endl;
+    }
+
+    for (int i = 1; i <= n; i++)
+    {
+        // space
+        for (int j = 1; j <= i - 1; j++)
+        {
+            cout << " ";
+        }
+
+        // star
+        for (int j = 1; j <= 2 * (n - i) + 1; j++)
+        {
+            cout << "*";
+        }
+        cout << endl;
+    }
+    return 0;
+}
