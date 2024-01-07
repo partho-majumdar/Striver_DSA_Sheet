852e850a109835f7ce8516733e7a75039d6a62f7
heipartho05 <heipartho72@gmail.com>
2024-01-07 21:20:49 +0600

    all divisor added

--- Basic_Math/all_divisor.cpp
+++ Basic_Math/all_divisor.cpp
@@ -5,12 +5,33 @@ int main()
 {
     int n = 36;
 
-    for (int i = 1; i <= n; i++)
+    // for (int i = 1; i <= n; i++)
+    // {
+    //     if (n % i == 0)
+    //     {
+    //         cout << i << " ";
+    //     }
+    // }
+
+    vector<int> ls;
+
+    for (int i = 1; i * i <= n; i++)
     {
         if (n % i == 0)
         {
-            cout << i << " ";
+            ls.push_back(i);
+            if ((n / i) != i)
+            {
+                ls.push_back(n / i);
+            }
         }
+    }
+
+    sort(ls.begin(), ls.end());
+
+    for (auto i = ls.begin(); i != ls.end(); i++)
+    {
+        cout << *i << " ";
     }
 
     return 0;
