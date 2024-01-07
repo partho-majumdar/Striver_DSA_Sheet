081339fdf18d9718b84cb53956de99b8be42e5cc
heipartho05 <heipartho72@gmail.com>
2024-01-07 23:35:38 +0600

    prime added

---
+++ Basic_Math/prime.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n = 5;
+    int count = 0;
+
+    for (int i = 1; (i * i) <= n; i++)
+    {
+        if (n % i == 0)
+        {
+            count++;
+            if ((n / i) != i)
+            {
+                count++;
+            }
+        }
+    }
+
+    cout << count;
+    return 0;
+}
+
