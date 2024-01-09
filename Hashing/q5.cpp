f486167a1264cdd75e154869e55fd0de07f58a38
heipartho05 <heipartho72@gmail.com>
2024-01-09 22:03:28 +0600

    q5 added

---
+++ Hashing/q5.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cout << "Enter of element: ";
+    cin >> n;
+    int arr[n];
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    // pre-store
+    map<int, int> mpp;
+    for (int i = 0; i < n; i++)
+    {
+        // int value = arr[i];
+        // mpp[value]++;
+        mpp[arr[i]]++;
+    }
+
+    // print map value
+    for (auto i : mpp)
+    {
+        cout << i.first << "-->" << i.second << endl;
+    }
+
+    // fetch
+    int q;
+    cout << "Enter number of query: ";
+    cin >> q;
+    while (q--)
+    {
+        int num;
+        cout << "Enter number to see its occurrence: ";
+        cin >> num;
+
+        cout << mpp[num] << endl;
+    }
+
+    return 0;
+}
+
+// if value 10^7+ then use map
