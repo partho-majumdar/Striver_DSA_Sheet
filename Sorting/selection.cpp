425f0abf981ef5a6f41e84e73af5107c4207e26a
heipartho05 <heipartho72@gmail.com>
2024-01-10 13:35:47 +0600

    selection sort added

---
+++ Sorting/selection.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n = 6;
+    int arr[n] = {13, 46, 24, 52, 20, 9};
+
+    for (int i = 0; i <= n - 2; i++)
+    {
+        int min = i;
+        for (int j = i; j <= n - 1; j++)
+        {
+            if (arr[j] < arr[min])
+            {
+                min = j;
+            }
+        }
+        swap(arr[i], arr[min]);
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+
+    return 0;
+}
+
+// get the minimum and swap it (put minimum value in front)
