92fe61b10195707da76eea68cbf5d67c03e9b656
heipartho05 <heipartho72@gmail.com>
2024-01-12 00:47:48 +0600

    q12 added

---
+++ Recursion/q12.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void printPath(string s, int r, int c)
+{
+    if (r == 1 && c == 1)
+    {
+        cout << s << endl;
+        return;
+    }
+
+    if (r > 1)
+    {
+        printPath(s + "D", r - 1, c);
+    }
+
+    if (c > 1)
+    {
+        printPath(s + "R", r, c - 1);
+    }
+}
+
+int main()
+{
+    int row = 3;
+    int col = 3;
+    printPath("", row, col);
+
+    return 0;
+}
+
+// print path
