9b1382f61f8f7e36f2edcc8d157fe16366e056b8
heipartho05 <heipartho72@gmail.com>
2024-01-13 01:07:15 +0600

    update

--- Linked_List/Array_to_LL.cpp
+++ Linked_List/Array_to_LL.cpp
@@ -22,12 +22,12 @@ public:
     }
 };
 
-Node *arrayToLinkedList(vector<int> arr)
+Node *arrayToLinkedList(vector<int> &arr)
 {
     Node *head = new Node(arr[0]);
     Node *mover = head;
 
-    for (int i = 0; i < arr.size(); i++)
+    for (int i = 1; i < arr.size(); i++)
     {
         Node *temp = new Node(arr[i]);
         mover->next = temp;
