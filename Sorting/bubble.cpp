1bdcd656759150b6b3e12531861140dcd6f93cf0
heipartho05 <heipartho72@gmail.com>
2024-02-05 13:05:10 +0600

    update

--- Sorting/bubble.cpp
+++ Sorting/bubble.cpp
@@ -6,10 +6,10 @@ int main()
     int n = 6;
     int arr[n] = {13, 46, 24, 52, 20, 9};
 
-    for (int i = n - 1; i >= 1; i--)
+    for (int i = 0; i <= n - 1; i++)
     {
         int didSwap = 0;
-        for (int j = 0; j <= i - 1; j++)
+        for (int j = 0; j <= n - 2 - i; j++)
         {
             if (arr[j] > arr[j + 1])
             {
@@ -31,4 +31,10 @@ int main()
     return 0;
 }
 
-// put maximum value in last by adjacent swap worst/avg T.C --> O(N^2) and best T.C --> O(N)
+/*
+
+put maximum value in last by adjacent swap worst/avg T.C --> O(N^2) and best T.C --> O(N)
+outer for loop run: 0 to n-1
+inner for loop run: 0 to n-2
+
+*/ 
