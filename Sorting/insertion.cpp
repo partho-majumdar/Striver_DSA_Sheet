5ace1f5a6f335875447ffea719a57ed570d74a3e
heipartho05 <heipartho72@gmail.com>
2024-02-05 13:05:22 +0600

    update

--- Sorting/insertion.cpp
+++ Sorting/insertion.cpp
@@ -34,4 +34,40 @@ sort till index 0
 Worst and avg T.C --> O(N^2)
 Best T.C --> O(N)
 
+#include <bits/stdc++.h>
+using namespace std;
+
+void insertionSort(int arr[], int n)
+{
+    for (int i = 0; i <= n - 2; i++)
+    {
+        for (int j = i + 1; j > 0; j--)
+        {
+            if (arr[j] < arr[j - 1])
+            {
+                swap(arr[j], arr[j - 1]);
+            }
+            else
+            {
+                break;
+            }
+        }
+    }
+}
+
+int main()
+{
+    int n = 5;
+    int arr[n] = {5, 4, 3, 2, 1};
+
+    insertionSort(arr, n);
+
+    for (int i = 0; i <= n - 1; i++)
+    {
+        cout << arr[i] << " ";
+    }
+
+    return 0;
+}
+
 */
